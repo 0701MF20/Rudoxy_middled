e41e67c0e1f80d7adc6842e10a284c519491388b
0701MF20 <farazmohd0701@gmail.com>
2021-05-26 22:09:22 +0530

    In middle way of Rudoxy app directory we have to resolve all problems

--- app/src/main/java/com/example/rudoxy/FamilymemberActivity.java
+++ app/src/main/java/com/example/rudoxy/FamilymemberActivity.java
@@ -30,23 +30,53 @@ public class FamilymemberActivity extends AppCompatActivity {
     }
 }*/
 package com.example.rudoxy;
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.ArrayList;
-
 public class FamilymemberActivity extends AppCompatActivity {
+    private MediaPlayer mp3;
+    private AudioManager am3;
+    private AudioManager.OnAudioFocusChangeListener audiofocuslistener3=new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT||focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
+            {
+             mp3.pause();
+             mp3.seekTo(0);
+            }
+            else if(focusChange==AudioManager.AUDIOFOCUS_LOSS)
+            {
+             ReleaseResource();
+            }
+            else if(focusChange==AudioManager.AUDIOFOCUS_GAIN)
+            {
+                mp3.start();
+            }
+        }
+    };
+    public void ReleaseResource()
+    {
+        if(mp3!=null)
+        {
+            mp3.release();
+            mp3=null;
+            am3.abandonAudioFocus(audiofocuslistener3);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstantstate)
     {
         super.onCreate(savedInstantstate);
         setContentView(R.layout.activity_familymember);
-        ArrayList<Word> fm=new ArrayList<Word>();
+        am3=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        final ArrayList<Word> fm=new ArrayList<Word>();
         fm.add(new Word("Father", "әpә","Pita",R.drawable.family_father,R.raw.family_father));
         fm.add(new Word("Mother", "әṭa","Maa",R.drawable.family_mother,R.raw.family_mother));
         fm.add(new Word("Son", "angsi","Beta",R.drawable.family_son,R.raw.family_son));
@@ -66,13 +96,35 @@ public class FamilymemberActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word w3 = fm.get(position);
                 int Audio = w3.getmResorceAudio();
-                MediaPlayer mp3 = MediaPlayer.create(FamilymemberActivity.this, Audio);
-                if (mp3.isPlaying()) {
+                int result3=am3.requestAudioFocus(audiofocuslistener3,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(result3==AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+                {
+                    mp3 = MediaPlayer.create(FamilymemberActivity.this, Audio);
+                   mp3.start();
+                /*if (mp3.isPlaying()) {
                     mp3.pause();
                 } else {
                     mp3.start();
                 }
+                */
+                }
             }
         });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ReleaseResource();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mp3.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mp3.pause();
     }
 }
